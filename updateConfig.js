5170715756ad4f2748b74c43c7b0c721c1265656
Yudjenn <yudjenrb@icloud.com>
2024-03-31 17:38:34 +0200

    crazy update

---
+++ updateConfig.js
@@ -0,0 +1,14 @@
+let config = {
+    update: {
+      autoStart: true,
+      roomName: '🟣⚫️⚽️ The SPL lobbies are getting updated, they will be back soon! ⚽️⚫️🟣',
+      playerName: 'wyd here brudda 💀',
+      token: 'thr1.AAAAAGYEXJeQw9VVLGXTKw.PxP2rQuj4Rc',
+      maxPlayers: 2,
+      geo: {'code': 'gb', 'lat': 50, 'lon': 3},
+      public: true,
+      noPlayer: true,
+      roomScript: './update.js'
+    }
+  };
+  module.exports = config;
